33866983e6651a6cb1cf1e7755e9837122bf5134
Jonathan Chu <jonathan.chu@me.com>
2013-09-13 18:09:47 -0400

    Changes to battery output in xmobarrc, xmodmap change to remap control to caps lock, and use gnome-terminal for default terminal software.

--- xmonad.hs
+++ xmonad.hs
@@ -43,7 +43,7 @@ myModMask            = mod4Mask       -- changes the mod key to "super"
 myFocusedBorderColor = "#ff0000"      -- color of focused border
 myNormalBorderColor  = "#cccccc"      -- color of inactive border
 myBorderWidth        = 1              -- width of border around windows
-myTerminal           = "terminator"   -- which terminal software to use
+myTerminal           = "gnome-terminal"   -- which terminal software to use
 myIMRosterTitle      = "Contact List" -- title of roster on IM workspace
 
 
